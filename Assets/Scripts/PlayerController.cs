a8d4b9d639c344737bef02787a2317df048a02dd
Emil Thorndal <Emilthorndal3@Gmail.com>
2017-01-12 18:04:11 +0100

    Roll a Ball 1.0
    Entire game
    - Restart Level

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed;
+
+	public Text winText;
+	public Text countText;
+	private Rigidbody rb;
+	private int count;
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		rb = GetComponent<Rigidbody> ();
+		SetCountText ();
+		winText.text = "";
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * Speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win!";
+		}
+	}
+}
---
+++ Assets/Scripts/Start_Game.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_Game : MonoBehaviour {
+
+	public GameObject pPlayer;
+	public GameObject pPanel;
+
+	public void StartWhenClicked()
+	{
+		if (pPlayer.active == false) {
+		pPlayer.SetActive(true);
+		pPanel.SetActive(false);
+			Cursor.visible = false;
+		}
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+}
