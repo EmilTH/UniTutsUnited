4ff2060524c1ca524ce8fea0267bc41103df2d07
Emil Thorndal <Emilthorndal3@Gmail.com>
2017-01-23 14:30:18 +0100

    sds
    sdsd

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count {
+
+		public int minimum;
+		public int maximum;
+
+		public Count (int min, int max)
+		{
+
+			minimum = min;
+			maximum = max;
+
+		}
+	}
+
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count (5,9);
+	public Count foodCount = new Count (1,5);
+	public GameObject exit;
+	public GameObject[] floorTiles;
+	public GameObject[] wallTiles;
+	public GameObject[] foodTiles;
+	public GameObject[] enemyTiles;
+	public GameObject[] outerwallTiles;
+
+	private Transform boardHolder;
+	private List <Vector3> gridPositions = new List<Vector3>();
+
+	void InitialiseList()
+	{
+		gridPositions.Clear ();
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
